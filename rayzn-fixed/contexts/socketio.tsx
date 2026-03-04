0b1f4c88553c075a5d81bd00475d7028f699e539
Rayzn <alibabapro040@gmail.com>
2026-03-04 10:20:30 +0300

    Delete rayzn-fixed/contexts directory

--- rayzn-fixed/contexts/socketio.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React, { createContext } from "react";
-
-const SocketContext = createContext(null);
-
-export default function SocketContextProvider({ children }: { children: React.ReactNode }) {
-  return <SocketContext.Provider value={null}>{children}</SocketContext.Provider>;
-}
